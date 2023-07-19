8f4fc5dce1079a01288a989136af172d43f5412e
Kenneth Estanislao <hacksider@gmail.com>
2023-07-19 11:21:38 +0800

    Revert "added some core enhancements"
    This reverts commit f67bd938fe95242114272e24207b1e67ff3345f8.

--- roop/face_analyser.py
+++ roop/face_analyser.py
@@ -1,10 +1,9 @@
 import threading
-from typing import Any, Optional, List
+from typing import Any
 import insightface
-import numpy
 
 import roop.globals
-from roop.typing import Frame, Face
+from roop.typing import Frame
 
 FACE_ANALYSER = None
 THREAD_LOCK = threading.Lock()
@@ -16,38 +15,20 @@ def get_face_analyser() -> Any:
     with THREAD_LOCK:
         if FACE_ANALYSER is None:
             FACE_ANALYSER = insightface.app.FaceAnalysis(name='buffalo_l', providers=roop.globals.execution_providers)
-            FACE_ANALYSER.prepare(ctx_id=0)
+            FACE_ANALYSER.prepare(ctx_id=0, det_size=(640, 640))
     return FACE_ANALYSER
 
 
-def clear_face_analyser() -> Any:
-    global FACE_ANALYSER
-
-    FACE_ANALYSER = None
-
-
-def get_one_face(frame: Frame, position: int = 0) -> Optional[Face]:
-    faces = get_many_faces(frame)
-    if faces:
-        try:
-            return faces[position]
-        except IndexError:
-            return faces[-1]
-    return None
-
-
-def get_many_faces(frame: Frame) -> Optional[List[Face]]:
+def get_one_face(frame: Frame) -> Any:
+    face = get_face_analyser().get(frame)
     try:
-        return get_face_analyser().get(frame)
+        return min(face, key=lambda x: x.bbox[0])
     except ValueError:
         return None
 
 
-def find_similar_face(frame: Frame, reference_face: Face) -> Optional[Face]:
-    faces = get_many_faces(frame)
-    for face in faces:
-        if hasattr(face, 'normed_embedding') and hasattr(reference_face, 'normed_embedding'):
-            distance = numpy.sum(numpy.square(face.normed_embedding - reference_face.normed_embedding))
-            if distance < roop.globals.similar_face_distance:
-                return face
-    return None
+def get_many_faces(frame: Frame) -> Any:
+    try:
+        return get_face_analyser().get(frame)
+    except IndexError:
+        return None
